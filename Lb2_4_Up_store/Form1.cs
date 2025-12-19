5faa736ffdf403de4e96aca9baf95f6c61e50282
Artem-Hub66 <aruporoff@yandex.ru>
2025-12-19 13:48:38 +0500

    Добавьте файлы проекта.

---
+++ Lb2_4_Up_store/Form1.cs
@@ -0,0 +1,10 @@
+namespace Lb2_4_Up_store
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Lb2_4_Up_store/Program.cs
@@ -0,0 +1,17 @@
+namespace Lb2_4_Up_store
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
