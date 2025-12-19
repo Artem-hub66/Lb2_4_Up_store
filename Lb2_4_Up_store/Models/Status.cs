402731d00cbad2d0cf5ef431c15599b16aacd07b
Artem-Hub66 <aruporoff@yandex.ru>
2025-12-19 14:15:35 +0500

    Установка пакетов: Microsoft.EntityFrameworkCore.Tools Npgsql.EntityFrameworkCore.PostgreSQL
    Подключение к базе данных

---
+++ Lb2_4_Up_store/Models/Status.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lb2_4_Up_store.Models;
+
+public partial class Status
+{
+    public int Id { get; set; }
+
+    public string StatusName { get; set; } = null!;
+
+    public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
+}
---
+++ Lb2_4_Up_store/Models/Supplier.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lb2_4_Up_store.Models;
+
+public partial class Supplier
+{
+    public int Id { get; set; }
+
+    public string SupplierName { get; set; } = null!;
+
+    public virtual ICollection<Product> Products { get; set; } = new List<Product>();
+}
