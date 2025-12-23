4dfded983aa1d2512471fee7f1a202f602a42bfc
Artem-Hub66 <aruporoff@yandex.ru>
2025-12-23 13:01:02 +0500

    Создание формы FormProducts

--- Lb2_4_Up_store/FormLogin.cs
+++ Lb2_4_Up_store/FormLogin.cs
@@ -1,11 +1,5 @@
 ﻿using Lb2_4_Up_store.Models;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Text;
-using System.Windows.Forms;
 
 namespace Lb2_4_Up_store
 {
---
+++ Lb2_4_Up_store/FormProducts.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Lb2_4_Up_store
+{
+    public partial class FormProducts : Form
+    {
+        public FormProducts()
+        {
+            InitializeComponent();
+        }
+    }
+}
